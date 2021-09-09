9eb09f8041bbe336b5ed2b3b7c2e34f548deead1
shiivaanii <shiivaanii97@gmail.com>
2021-09-09 21:46:53 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import candidateData from './candidate.json';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+interface Candidate {
+  id: Number;
+  first_name: String;
+  last_name: String;
+  email: String;
+  gender: String;
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  constructor(private formBuilder: FormBuilder) {}
+  candidateForm = this.formBuilder.group({
+    first_name: '',
+    last_name: '',
+    email: '',
+    gender:''
+  });
+  searchword: any;
+
+  candidates: Candidate[] = candidateData;
+
+  searchThis() {}
+
+  onSubmit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
